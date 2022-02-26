4601ebf9097b748a3f8712b68f09812cd5f9b84b
Kelgors <matthieuboheas@pm.me>
2022-02-26 11:54:45 +0100

    fix syntax and conditions in bash

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ mkdir -p $HOME/Build/ && cd $HOME/Build/
 
 echo "Welcome to you new environment"
 
-if [ -z $(which fzf) ];
+if [[ ! -f $(which fzf) ]];
 then
 echo "  Installing dependencies"
 sudo apt update -q
@@ -13,20 +13,21 @@ sudo apt upgrade -y
 
 sudo apt install -y \
   build-essential git curl wget htop tar # standard deps \
-  fzf bat silversearcher-ag ripgrep # install fzf-vim deps
+  fzf bat silversearcher-ag ripgrep # install fzf-vim deps \
+  fish
 fi
 
-echo "  Installing Rust"
-if [ ! -d $HOME/.rustup ];
+if [[ ! -d $HOME/.rustup ]];
 then
+  echo "  Installing Rust"
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 else
-  echo "    Rust already installed"
+  echo "  Rust already installed"
 fi
 
-echo "  Installing Node $NODE_VERSION"
-if [ ! -d $HOME/Build/node-$NODE_VERSION ];
+if [[ ! -d $HOME/Build/node-$NODE_VERSION ]];
 then
+  echo "  Installing Node $NODE_VERSION"
   wget https://nodejs.org/dist/$NODE_VERSION/node-$noe_version.tar.xz
   tar xf node-$NODE_VERSION.tar.xz
   rm node-$NODE_VERSION.tar.xz
@@ -36,19 +37,18 @@ else
   echo "  Node $NODE_VERSION already installed"
 fi
 
-
-echo "  Installing nvim"
-if [ -z $(which nvim) ];
+if [[ ! -f $(which nvim) ]];
 then
+  echo "  Installing nvim"
   sudo apt install -y neovim
 fi
 # add vim-plug
-sh 'curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c "curl -sfLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
 # local directory
-[ ! -d $HOME/.local ] && mkdir -p $HOME/.local/share
-[ ! -d $HOME/.local/share ] && mkdir $HOME/.local/share
-[ -d $HOME/.local/share/fonts ] && rm -r $HOME/.local/share/fonts
+[[ ! -d $HOME/.local ]] && mkdir -p $HOME/.local/share
+[[ ! -d $HOME/.local/share ]] && mkdir $HOME/.local/share
+[[ -d $HOME/.local/share/fonts ]] && rm -r $HOME/.local/share/fonts
 ln -sf $path/local/share/fonts $HOME/.local/share/fonts
 
 echo "  Setting config up"
@@ -56,7 +56,7 @@ config_folders="alacritty nvim"
 
 for config_folder in $config_folders
 do
-  if [ -d $HOME/.config/$config_folder ];
+  if [[ -d $HOME/.config/$config_folder ]];
   then
     echo "    Remove .config/$config_folder directory"
     rm -rf $HOME/.config/$config_folder
@@ -65,7 +65,7 @@ do
   ln -sf $path/config/$config_folder $HOME/.config/$config_folder
 done
 
-if [ ! -f $HOME/.kelgors.profile ];
+if [[ ! -f $HOME/.kelgors.profile ]];
 then
   echo "Creating symlink for .kelgors.profile"
   ln -sf $path/kelgors.profile $HOME/.kelgors.profile
--- install_alacritty.sh
+++ install_alacritty.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 echo "Installing Alacritty"
-alacritty_path=$(which alactritty)
+alacritty_path=$(which alacritty)
 
-if [ ! -f $alacritty_path ];
+if [[ ! -f $alacritty_path ]];
 then
   echo "Install dependencies"
-  exit 0
   sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
   echo "Cloning repo"
   mkdir -p $HOME/Build/ && cd $HOME/Build/
@@ -14,10 +13,8 @@ then
   cd alacritty
 else
   echo " Updating alacritty"
-  exit 0
   cd $HOME/Build/alacritty
   git pull
-  exit 0
 fi
 
 echo "Building Alacritty"
@@ -25,14 +22,15 @@ cargo build --release
 strip -s target/release/alacritty
 
 echo "Add Desktop Entry"
-if [ ! -f $alacritty_path ];
+if [[ ! -f $alacritty_path ]];
+then
   sudo cp target/release/alacritty /usr/local/bin
   sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
   sudo desktop-file-install extra/linux/Alacritty.desktop
   sudo update-desktop-database
   echo "Alacritty is now installed."
 else
-  sudo cp target/release/alacritty $alacritty_path || echo "You're using Alacritty right now. Run this command to update Alacritty from another terminal\n  sudo cp target/release/alacritty $alacritty_path"
+  sudo cp target/release/alacritty $alacritty_path || echo -e "You're using Alacritty right now. Run this command to update Alacritty from another terminal\n  sudo cp target/release/alacritty $alacritty_path"
   echo "Alacritty is now updated"
 fi
 
