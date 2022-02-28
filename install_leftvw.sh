3608efdca4eca8e98ca50b3b1ae92af03b8e4349
Matthieu BOHEAS <matthieu.boheas@digital4better.com>
2022-02-28 13:44:08 +0100

    remove lightdm

--- install_leftvw.sh
+++ install_leftvw.sh
@@ -4,7 +4,7 @@ if [[ ! -f $(which feh) ]];
 then
   echo "Installing dependencies"
   sudo apt install \
-    x11-xserver-utils xorg lightdm \
+    x11-xserver-utils xorg \
     libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev \
     libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev \
     libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev \
