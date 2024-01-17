356cee7458668511e17ed6e4f7b4c4d11dd93658
Kelgors <kelgors@ssh.club>
2024-01-17 22:37:27 +0100

    Add catpuccin

--- install.zsh
+++ install.zsh
@@ -9,3 +9,5 @@ stow -v htop
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$DEPDIR/zsh/syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git "$DEPDIR/zsh/autosuggestions"
 git clone https://github.com/catppuccin/zsh-syntax-highlighting.git "$DEPDIR/catppuccin/zsh-syntax-highlighting"
+# tmux deps
+git clone https://github.com/tmux-plugins/tpm "$DEPDIR/tmux/tpm"
