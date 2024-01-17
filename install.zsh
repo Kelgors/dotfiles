c5f77c6dc23ac7b1edb058c68b70293a97fcc60d
Kelgors <kelgors@ssh.club>
2024-01-17 22:04:42 +0100

    Add install script

---
+++ install.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/zsh -eu
+DEPDIR="$HOME/.local/share"
+
+stow -v nvim
+stow -v zsh
+stow -v htop
+
+# zsh dependencies
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$DEPDIR/zsh/syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "$DEPDIR/zsh/autosuggestions"
+git clone https://github.com/catppuccin/zsh-syntax-highlighting.git "$DEPDIR/catppuccin/zsh-syntax-highlighting"
