e0414735ef090e3b5f1beb6a0044e0c6ddcc4b02
Kelgors <kelgors@ssh.club>
2024-01-20 09:02:06 +0100

    Fix neovim theme

--- nvim/.config/nvim/lua/kelgors/plugins.lua
+++ nvim/.config/nvim/lua/kelgors/plugins.lua
@@ -80,7 +80,11 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function ()
-      require('lualine').setup()
+      require('lualine').setup({
+        options = {
+          theme = "catppuccin"
+        }
+      })
     end,
   },
   {
@@ -121,7 +125,36 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function ()
-      require("catppuccin").setup({ flavour = "mocha" })
+      require("catppuccin").setup({ 
+        flavour = "mocha",
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          treesitter = true,
+          mason = true,
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+                errors = { "italic" },
+                hints = { "italic" },
+                warnings = { "italic" },
+                information = { "italic" },
+            },
+            underlines = {
+                errors = { "underline" },
+                hints = { "underline" },
+                warnings = { "underline" },
+                information = { "underline" },
+            },
+            inlay_hints = {
+                background = true,
+            },
+          },
+          telescope = {
+            enabled = true,
+          }
+        }
+      })
     end
   },
 }
