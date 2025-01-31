e975d492ee342811ecb8ad00f61f3fbc6505295d
Kelgors <matthieuboheas@pm.me>
2025-01-31 10:07:08 +0100

    add autoload feature

--- home/zsh/.config/zsh/main.zsh
+++ home/zsh/.config/zsh/main.zsh
@@ -5,18 +5,24 @@ export ZSH_CONFIG_PATH="$XDG_CONFIG_HOME/zsh"
 export MANPATH="$XDG_DATA_HOME/man:$MANPATH"
 
 autoload=(
-  bindings
-  bin
-  rc
-  aliases
-  user
+  bindings.zsh
+  bin.zsh
+  rc.zsh
+  aliases.zsh
+  user.zsh
 )
 
+if [ -d "$ZSH_CONFIG_PATH/autoload" ]; then
+  for filename in $ZSH_CONFIG_PATH/autoload/*; do
+    autoload+=(${filename//$ZSH_CONFIG_PATH\//})
+  done
+fi
+
 for filename in "${autoload[@]}"; do
-  if [ -f "$ZSH_CONFIG_PATH/$filename.zsh" ]; then
-    source "$ZSH_CONFIG_PATH/$filename.zsh"
+  if [ -f "$ZSH_CONFIG_PATH/$filename" ]; then
+    source "$ZSH_CONFIG_PATH/$filename"
   else
-    echo "Unable to load $ZSH_CONFIG_PATH/$filename.zsh !"
+    echo "Unable to load $ZSH_CONFIG_PATH/$filename !"
   fi
 done
 
--- scripts/04-cli.sh
+++ scripts/04-cli.sh
@@ -1,6 +1,22 @@
 stow starship
 stow atuin
 
+# bat => cat
+# eza => ls
+# skim => fzf
+# fd-find => find
+# ripgrep => grep
+# bottom => top
+# tealdeer => tldr
+# delta => diff
+# du-dust => du
+# sd => sed
+# starship (shell prompt)
+# atuin (history in db)
+# tokei (stats about filetypes)
+# bandwhich (top for network)
+# grex (create regex from examples)
+
 packages=(
   bat
   eza
@@ -10,8 +26,12 @@ packages=(
   bottom
   starship
   atuin
+  tokei
+  tealdeer
 )
 for package in "${packages[@]}"; do
   echo "Installing $package..."
   cargo install "$package" --locked
 done
+
+tldr --update
